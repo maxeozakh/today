54c3f3ba98e59ca1a77c619997bd775e9ff1aa93
Maxim Zakharov <maxeozakh@gmail.com>
2021-12-26 10:02:03 +0300

    prototype for columnmaker

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@
 
 void get_rows_from_txt(const char *path, std::string *out_rows);
 void split_rows_to_tasks_and_times(const std::string *rows, std::string *out_tasks, std::string *out_times);
+void format_data_for_column(const std::string *raw_data, std::string *out_formatted_data, int &column_length);
 
 /*
-void format_data_for_text_column(const char *raw_data, char *out_formatted_data);
-void format_data_for_time_column(const char *raw_data, char *out_formatted_data);
 const char *make_row(const char *task_text,
 					 const char *task_time,
 					 const char *routine_text,
