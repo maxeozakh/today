2050e115274a206cd3ccb12a150e3a98545c78e3
Maxim Zakharov <maxeozakh@gmail.com>
2021-12-27 09:34:07 +0300

    format data for column

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,49 @@
 #include <string>
 
 void get_rows_from_txt(const char *path, std::string *out_rows);
-void split_rows_to_tasks_and_times(const std::string *rows, std::string *out_tasks, std::string *out_times);
-void format_data_for_column(const std::string *raw_data, std::string *out_formatted_data, int &column_length);
+void split_rows_to_tasks_and_times(const std::string *rows,
+								   std::string *out_tasks,
+								   std::string *out_times);
+void format_data_for_column(const std::string *raw_data,
+							std::string *out_formatted_data,
+							const int &column_length);
+
+int main()
+{
+	const int max_rows = 100;
+
+	std::string rows[max_rows];
+	get_rows_from_txt("../assets/tasks.txt", rows);
+
+	std::string tasks[max_rows], times[max_rows];
+	split_rows_to_tasks_and_times(rows, tasks, times);
+
+	std::string formatted_tasks[max_rows], formatted_times[max_rows];
+	format_data_for_column(tasks, formatted_tasks, 30);
+	format_data_for_column(times, formatted_times, 5);
+
+	std::cout << "🐕" << '\n';
+}
+
+void format_data_for_column(const std::string *raw_data, std::string *out_formatted_data, const int &column_length)
+{
+	std::string row, formatted_row;
+	while (*raw_data != "")
+	{
+		row = *raw_data;
+		int additional_spaces_count = column_length - row.length();
+
+		if (additional_spaces_count < 0)
+		{
+			std::cout << "oooooops, raw row is too long, dont wanna format that \n";
+			additional_spaces_count = 0;
+		}
+
+		formatted_row = row.append(additional_spaces_count, ' ');
+		std::cout << formatted_row << '\n';
+		raw_data++;
+	}
+}
 
 /*
 const char *make_row(const char *task_text,
@@ -18,20 +59,6 @@ void print_rows(const char *tasks_text,
 
 task-text --- t-time --- r-text --- r-time 
 */
-
-int main()
-{
-	const int max_rows = 100;
-
-	std::string rows[max_rows];
-	get_rows_from_txt("../assets/tasks.txt", rows);
-
-	std::string tasks[max_rows];
-	std::string times[max_rows];
-	split_rows_to_tasks_and_times(rows, tasks, times);
-
-	std::cout << "🐕" << '\n';
-}
 
 void split_rows_to_tasks_and_times(const std::string *rows, std::string *out_tasks, std::string *out_times)
 {
